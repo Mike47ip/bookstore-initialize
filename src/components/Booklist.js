309846d42006c4b48a9250e6dcef0d53c337641a
Mike Dark <agyemankojo47@gmail.com>
2023-09-01 11:32:48 +0000

    Fetch data from api

--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -1,21 +1,48 @@
-import React from 'react';
+/* eslint-disable camelcase */
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
 import Books from './Books';
-import { removeBook } from '../redux/books/booksSlice';
+import { getBooks, deleteBookFromApi } from '../redux/books/booksSlice';
 
 function BookList() {
   const booksArr = useSelector((state) => state.book.books);
+  const booksStatus = useSelector((state) => state.book.status);
+  const booksError = useSelector((state) => state.book.error);
+
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getBooks());
+  }, [dispatch]);
+
+  const transformedData = Object.entries(booksArr).map(([item_id, items]) => {
+    const [item] = items;
+    return { item_id, ...item };
+  });
+
   const handleChange = (e) => {
-    dispatch(removeBook(e.target.id));
+    dispatch(deleteBookFromApi(e.target.id));
   };
+
+  if (booksStatus === 'loading') {
+    return (
+      <h1 style={{ textAlign: 'center' }}>Loading...</h1>
+    );
+  }
+
+  if (booksError !== null) {
+    return (
+      <h1 style={{ textAlign: 'center' }}>{booksError}</h1>
+    );
+  }
 
   return (
     <>
-      {booksArr.map((book) => (
+      {transformedData.map((book) => (
         <Books
-          key={book.itemId + book.title}
-          id={book.itemId}
+          key={uuidv4()}
+          id={book.item_id}
           category={book.category}
           title={book.title}
           author={book.author}
--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,51 +1,82 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+// MbLdQuSA291U7W3VkOff
+
+const baseUrl = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/MyUuH2oQoI6TQXsG9GmZ/books';
+
+export const getBooks = createAsyncThunk(
+  'books/getBooks',
+  async () => {
+    const res = await axios(baseUrl);
+    return res.data;
+  },
+);
+
+export const postBook = createAsyncThunk(
+  'books/postBook',
+  async (book, thunkAPI) => {
+    try {
+      await axios.post(baseUrl, book);
+      thunkAPI.dispatch(getBooks());
+      const res = thunkAPI.getState().books;
+      return res;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(
+        error?.data?.message || 'Sorry Error Occured!',
+      );
+    }
+  },
+);
+
+export const deleteBookFromApi = createAsyncThunk(
+  'books/deleteBookFromApi',
+  async (id, thunkAPI) => {
+    try {
+      await axios.delete(`${baseUrl}/${id}`);
+      thunkAPI.dispatch(getBooks());
+      const res = thunkAPI.getState().books;
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(
+        error?.data?.message || 'Something went wrong!',
+      );
+    }
+  },
+);
 
 const initialState = {
-  books: [
-    {
-      itemId: 'item1',
-      title: 'Matrix',
-      author: 'Wiil Smith',
-      category: 'Fiction',
-      progress: '84',
-    },
-    {
-      itemId: 'item2',
-      title: 'The Avatar',
-      author: 'Kwaku Johnson',
-      category: 'Fiction',
-      progress: '28',
-    },
-    {
-      itemId: 'item3',
-      title: 'The Selfish Gene',
-      author: 'Kwame Atta',
-      category: 'Romance',
-      progress: '5',
-    },
-    {
-      itemId: 'item4',
-      title: 'God of War',
-      author: 'Mike Dawkins',
-      category: 'Action',
-      progress: '73',
-    },
-  ],
+  books: [],
+  status: 'idle',
+  error: null,
 };
 
 const bookSlice = createSlice({
   name: 'books',
   initialState,
   reducers: {
-    addBook: (state, action) => {
-      state.books.push(action.payload);
-    },
-    removeBook: (state, action) => {
-      const bookId = action.payload;
-      state.books = state.books.filter((book) => book.itemId !== bookId);
-    },
+
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getBooks.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(getBooks.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.books = action.payload;
+      })
+      .addCase(getBooks.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      })
+      .addCase(postBook.fulfilled, (state, action) => {
+        Object.assign(state.books, action.payload);
+      })
+      .addCase(deleteBookFromApi.fulfilled, (state, action) => {
+        delete state.books[action.payload];
+      });
   },
 });
 
-export const { addBook, removeBook } = bookSlice.actions;
 export default bookSlice.reducer;
